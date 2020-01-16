feedc65ae36825e635a4c88a56f24f742fe30791
Alec Venzor <alecvenzoredu@gmail.com git config --global user.name Alec Venzorgit config --global user.email alecvenzoredu@gmail.com>
2020-01-15 19:19:34 -0800

    lowercased?

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === "production")
 {
-    module.exports = {mongoURI: "mongodb+srv://Alec:Alec@vidjot-jn7qd.gcp.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://Alec:alec@vidjot-jn7qd.gcp.mongodb.net/test?retryWrites=true&w=majority"}
 }
 else
 {
