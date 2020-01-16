ef9ceb2cef6b147b9a959adb9617d555bc7d2e0b
Alec Venzor <alecvenzoredu@gmail.com git config --global user.name Alec Venzorgit config --global user.email alecvenzoredu@gmail.com>
2020-01-15 19:34:46 -0800

    ,.

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
-if(process.env.NODE_ENV === "production")
-{
+
     module.exports = {mongoURI: "mongodb+srv://Alec:Alec@vidjot-jn7qd.gcp.mongodb.net/test?retryWrites=true&w=majority"}
-}
-else
-{
-module.exports = {mongoURI:"mongodb://localhost/vidjot-dev"}
-}
+
+// else
+// {
+// module.exports = {mongoURI:"mongodb://localhost/vidjot-dev"}
+// }
